81924f8191fe8f7549db26cc4c18a8de4e0e5533
Archisman Dey <archismandey13@gmail.com>
2019-11-04 11:54:11 +0530

    Bug Fixes

--- spotify.py
+++ spotify.py
@@ -1,4 +1,9 @@
-# Arduino_LED_user.py
+"""
+Tasks left :
+1. If bot is very close to the edge,right now it might go outside boundary while moving. Fix this.
+2. Detect for collisions, if two bots get very near, the one with bot_number less should go back (but not outside boundary) and let the bot with more bot_number go first
+3. Right now, if one of the bots shows NA, it just goes back. This might not work with edge cases.
+"""
 
 import serial
 import time
@@ -15,12 +20,17 @@ delay_after_send = 2
 # 640 by 480, angle between 0 and 360
 # Define the serial port and baud rate.
 # Ensure the 'COM#' corresponds to what was seen in the Windows Device Manager
-url = "http://10.150.39.190:8080/data.txt"
+#url = "http://10.150.39.190:8080/data.txt"
+url = "http://192.168.137.9:8080/data.txt"
 ser = serial.Serial('COM6', 9600)
+print("Connected to Serial")
 
 def get_coordinates ():
+	print("Get coordinates called")
 	r = requests.get(url)
 	string = (str)(r.content).strip("b'")
+
+	print(string)
 
 	initial_temp = (string.split('=')[0]).split('-')
 	final_temp = (string.split('=')[1]).split('-')
@@ -30,42 +40,62 @@ def get_coordinates ():
 		final[i] = [final_temp[i].split(',')[0] , final_temp[i].split(',')[1]]
 
 def angle (xi, yi, xf, yf):
-	if(xf == xi):
-		if(yi > yf):
-			return 90
+	print("Finding angle between : ")
+	print(xi, yi, xf, yf)
+	result = 0
+	if xf == xi:
+		if yi > yf:
+			result = 90
 		else:
-			return 270
-	if (xi >= xf and yf >= yi):
-		return math.degrees(math.atan((yf-yi)/(xi-xf))) + 180
+			result = 270
+	elif (xi >= xf and yf >= yi):
+		result = math.degrees(math.atan((yf-yi)/(xi-xf))) + 180
 	elif (yf >= yi and xf >= xi):
-		return 360 - math.degrees(math.atan((yf-yi)/(xf-xi)))
+		result = 360 - math.degrees(math.atan((yf-yi)/(xf-xi)))
 	elif (xf >= xi and yi >= yf):
-		return math.degrees(math.atan((yi-yf)/(xf-xi)))
+		result = math.degrees(math.atan((yi-yf)/(xf-xi)))
 	elif (xi >= xf and yi >= yf):
-		return 180 - math.degrees(math.atan((yi-yf)/(xi-xf)))
+		result = 180 - math.degrees(math.atan((yi-yf)/(xi-xf)))
+	print("Angle is ")
+	print(result)
+	return result
 
 def convert_coordinates_to_int ():
+	na = []
 	for i in range (0, 4, 1):
 		for j in range (0, 3, 1):
 			if initial[i][j] == 'NA':
-				react_to_NA(i + 1)
+				na.append(i + 1)
+				break
 			initial[i][j] = (int)(initial[i][j])
 	for i in range (0, 4, 1):
 		for j in range (0, 2, 1):
 			final[i][j] = (int)(final[i][j])
 
-def react_to_NA(bot_number):
+	if not na == []:
+		react_to_NA(na)
+
+def react_to_NA(bot_number_list):
+	print("Bot " + (str)(bot_number_list) + " is showing NA")
+	message = [5]*4
+	for bot_number in bot_number_list:
+		message[bot_number - 1] = 4
+	send_to_bots(message)
+	get_coordinates()
+	convert_coordinates_to_int()
 
 
 def distancesq (l1, l2):
 	return (l1[0] - l2[0])**2 + (l1[1]-l2[1])**2
 
 def send_to_bots (num_list):
+	print("Sending : " + (str)(num_list))
 	message = (bytes)(''.join([str(elem) for elem in num_list]), encoding = 'utf-8')
 	ser.write(message)
 	time.sleep(delay_after_send)
 
 def assign_bots ():
+	print("Assign bots called")
 	bots_temp = ()
 	mindist = 23746273649236487237627364
 	for i in range (1, 5, 1):
@@ -77,10 +107,13 @@ def assign_bots ():
 						if (dist < mindist):
 							mindist = dist
 							bots_temp = (i, j, k, l)
+	print(bots_temp)
 	return bots_temp
 
-def orient ():
+def orient (already_done):
 	message = [5]*4
+	global max_tries_orient
+	print("Max tries orient is " + (str)(max_tries_orient))
 	tries = max_tries_orient
 	while tries:
 		get_coordinates()
@@ -116,14 +149,20 @@ def orient ():
 				if not done[bot_number - 1]:
 					message[bot_number - 1] = 2
 
+		print(already_done)
+		for bot_number in range (1, 5, 1):
+			if already_done[bot_number - 1]:
+				message[bot_number - 1] = 6
+	
 		send_to_bots(message)
 		tries -= 1
 
 	max_tries_orient = 5
 
-
-def move_forward_a_bit (bot_number):
+def move_forward_a_bit (done):
 	message = [3]*4
+	global max_tries_forward
+	print("Max tries forward is " + (str)(max_tries_forward))
 	tries = max_tries_forward
 
 	get_coordinates()
@@ -132,6 +171,10 @@ def move_forward_a_bit (bot_number):
 	initial_distances = [distancesq(initial[bot_number - 1] , final[bots[bot_number - 1] - 1]) for bot_number in range(1, 5, 1)]
 
 	while tries:
+		for bot_number in range (1, 5, 1):
+			if done[bot_number - 1]:
+				message[bot_number - 1] = 5
+
 		send_to_bots(message)
 
 		get_coordinates()
@@ -139,20 +182,25 @@ def move_forward_a_bit (bot_number):
 
 		distances = [distancesq(initial[bot_number - 1] , final[bots[bot_number - 1] - 1]) for bot_number in range(1, 5, 1)]
 
-		for bot_number in (1, 5, 1):
+		for bot_number in range(1, 5, 1):
 			if initial_distances[bot_number - 1] - distances[bot_number - 1]:
-				message[bot_number - 1] = 5
+				message[bot_number - 1] = 6
 
 		tries -= 1
 
 def check_if_done ():
+	print("Checking if done")
+	message = [5]*4
 	get_coordinates()
 	convert_coordinates_to_int()
 	done = [False, False, False, False]
 	for i in range (0, 4, 1):
 		if distancesq(initial[i], final[bots[i] - 1]) < 9:
 			done[i] = True
-			send_to_bot(i, 6)
+			message[i] = 6
+	if not message == [5]*4:
+		send_to_bots(message)
+	print(done)
 	return done
 
 time.sleep(2) # wait for the serial connection to initialize
@@ -163,24 +211,7 @@ bots = assign_bots()
 
 while True:
 	done = check_if_done()
+	orient(done)
+	move_forward_a_bit(done)
 
-	for i in range (1, 5, 1):
-		if done[i - 1] == True:
-			continue
-		orient(i)
-		move_forward_a_bit(i)
-
-"""
-for i in range (1, 6, 1):
-	send_to_bot(4, 3)
-	time.sleep(1.25)
-
-for i in range (1, 6, 1):
-	send_to_bot(1,3)
-	time.sleep(1.5)
-	send_to_bot(2,3)
-	time.sleep(1.5)
-	send_to_bot(3,3)
-	time.sleep(1.5)
-"""
 ser.close()
